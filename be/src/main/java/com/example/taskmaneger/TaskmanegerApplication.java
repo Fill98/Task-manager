01fb94f4ac659a53b1d48841290f924a57406694
ffabo <ffabo@student.umb.sk>
2026-06-12 15:03:52 +0200

    Spring boot ini

---
+++ be/src/main/java/com/example/taskmaneger/TaskmanegerApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmaneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanegerApplication.class, args);
+	}
+
+}
