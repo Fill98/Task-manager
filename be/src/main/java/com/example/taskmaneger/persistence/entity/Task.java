2a45bf9fbe6ee2776e3dadbcbfecdfaf5be3967d
ffabo <ffabo@student.umb.sk>
2026-06-15 20:39:52 +0200

    Created Household

--- be/src/main/java/com/example/taskmaneger/persistence/entity/Task.java
+++ be/src/main/java/com/example/taskmaneger/persistence/entity/Task.java
@@ -24,6 +24,10 @@ public class Task {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @ManyToOne
+    @JoinColumn(name = "household_id")
+    private Household household;
+
     private String taskName;
     private String description;
 
