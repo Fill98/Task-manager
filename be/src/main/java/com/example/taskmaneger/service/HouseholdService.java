431d498ad1f12be683c179f6a65a53227a9da300
Martin <matohtk@gmail.com>
2026-07-01 17:10:36 +0200

    metoda na vytvorenie domacnosti pridana

--- be/src/main/java/com/example/taskmaneger/service/HouseholdService.java
+++ be/src/main/java/com/example/taskmaneger/service/HouseholdService.java
@@ -1,6 +1,11 @@
 package com.example.taskmaneger.service;
 
+import com.example.taskmaneger.dtos.householddto.CreateHouseholdDto;
+import com.example.taskmaneger.dtos.householddto.HouseholdDto;
+import com.example.taskmaneger.persistence.entity.Household;
+import com.example.taskmaneger.persistence.entity.User;
 import com.example.taskmaneger.persistence.repository.HouseholdRepository;
+import com.example.taskmaneger.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +14,23 @@ public class HouseholdService {
 
     @Autowired
     private HouseholdRepository householdRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    //metoda na vytvorenie domacnosti
+    public HouseholdDto createHousehold(CreateHouseholdDto createHouseholdDto){
+        Household household = new Household();
+        household.setName(createHouseholdDto.name());
+
+        User owner =
+                userRepository.findById(createHouseholdDto.ownerId())
+                .orElseThrow(() -> new RuntimeException("owner household not found"));
+        household.setOwner(owner);
+        householdRepository.save(household);
+
+        return new HouseholdDto(household.getId(), owner.getId(), household.getName());
+    }
+
 
 
 }
