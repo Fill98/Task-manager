87579d11f1ed90a0db31938506b9a3810ae2faeb
ffabo <ffabo@student.umb.sk>
2026-07-01 08:37:28 +0200

    Created new Dto, and added Status to old DTOS

--- be/src/main/java/com/example/taskmaneger/dtos/taskdto/TaskDto.java
+++ be/src/main/java/com/example/taskmaneger/dtos/taskdto/TaskDto.java
@@ -1,8 +1,9 @@
 package com.example.taskmaneger.dtos.taskdto;
 
 import com.example.taskmaneger.persistence.entity.Priority;
+import com.example.taskmaneger.persistence.entity.Status;
 
 import java.time.LocalDateTime;
 
-public record TaskDto(Long id, String taskName, String description, String assignedTo, String assignedBy, LocalDateTime mustBeDone, Priority priority) {
+public record TaskDto(Long id, String taskName, String description, String assignedTo, String assignedBy, LocalDateTime mustBeDone, Priority priority, Status status) {
 }
