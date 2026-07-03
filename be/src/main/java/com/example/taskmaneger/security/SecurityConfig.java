e51dbc9cefdfa3cc249bdbefdbe115122fa947ce
ffabo <ffabo@student.umb.sk>
2026-07-03 17:24:17 +0200

    basic security for now

--- be/src/main/java/com/example/taskmaneger/security/SecurityConfig.java
+++ be/src/main/java/com/example/taskmaneger/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.taskmaneger.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,8 +16,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http
                 .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/user").permitAll()
                         .anyRequest().authenticated());
         return http.build();
     }
