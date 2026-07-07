5ff1c28caae08db922e99637e0bc97a7281ed0ea
ffabo <ffabo@student.umb.sk>
2026-07-07 19:58:14 +0200

    zbytocny Status status, kedze aj tak natvrdo ho pri vytvarani nastavime na TODO

--- be/src/main/java/com/example/taskmaneger/dtos/taskdto/CreateTaskDto.java
+++ be/src/main/java/com/example/taskmaneger/dtos/taskdto/CreateTaskDto.java
@@ -3,10 +3,9 @@ package com.example.taskmaneger.dtos.taskdto;
 
 
 import com.example.taskmaneger.persistence.entity.Priority;
-import com.example.taskmaneger.persistence.entity.Status;
 
 import java.time.LocalDateTime;
 
-public record CreateTaskDto(String taskName, String description, Long userId, Long assignedById, LocalDateTime mustBeDone, Priority priority, Status status, Long householdId) {
+public record CreateTaskDto(String taskName, String description, Long userId, Long assignedById, LocalDateTime mustBeDone, Priority priority, Long householdId) {
 
 }
