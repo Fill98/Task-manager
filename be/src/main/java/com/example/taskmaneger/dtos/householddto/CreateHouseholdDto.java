6c9e3c76c801a40746e68ac07a12274825d8f09a
ffabo <ffabo@student.umb.sk>
2026-07-10 15:32:26 +0200

    Household autorizacia - create/addMember/removeMember cez prihlaseneho usera a odstranenie ownerID z createHousehold

--- be/src/main/java/com/example/taskmaneger/dtos/householddto/CreateHouseholdDto.java
+++ be/src/main/java/com/example/taskmaneger/dtos/householddto/CreateHouseholdDto.java
@@ -2,5 +2,5 @@ package com.example.taskmaneger.dtos.householddto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record CreateHouseholdDto (@NotBlank(message = "Household name must not by empty") String name, Long ownerId){
+public record CreateHouseholdDto (@NotBlank(message = "Household name must not by empty") String name){
 }
