2333114a74ec667d46623e2d44743592dc0780f1
ffabo <ffabo@student.umb.sk>
2026-07-13 20:40:47 +0200

    najdeny bug

--- be/src/main/java/com/example/taskmaneger/dtos/taskdto/ModifyTaskDto.java
+++ be/src/main/java/com/example/taskmaneger/dtos/taskdto/ModifyTaskDto.java
@@ -1,9 +1,11 @@
 package com.example.taskmaneger.dtos.taskdto;
 
 import com.example.taskmaneger.persistence.entity.Priority;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 
 import java.time.LocalDateTime;
 
-public record ModifyTaskDto(String taskName, String description, Long assignedTo,  LocalDateTime mustBeDone, Priority priority) {
+public record ModifyTaskDto(@NotBlank(message = "Task name must not be empty.") String taskName, String description, @NotNull Long assignedTo, LocalDateTime mustBeDone, @NotNull Priority priority) {
 }
